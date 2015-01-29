d16a22caf568845192fadf7ffd7b1ae1cedb7181
greg <gregorio.jp.mitchell@gmail.com>
2015-01-28 22:24:30 -0700

    added vid2 stuff.  Doesn't display local storage yet.

---
+++ vid2/demo.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    document.getElementById('output').innerHTML = 'test'; 
+};
+
+function saveDemoInput() {
+    localStorage.setItem('demoText',document.getElementsByName('demo-input')[0].value);
+};
+
+function clearLocalStorate(){
+    localStorage.clear();
+}
+
+
+function displayLocalStorage() {
+    document.getElementById('output').innerHTML = localStorage.getItem('demoText');
+}
