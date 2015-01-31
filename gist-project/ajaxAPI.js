5cc0d6904c8b40ec1f13bd487ae959ea41275236
greg <gregorio.jp.mitchell@gmail.com>
2015-01-31 05:37:53 -0700

    Sorta works asynchronously, but it's really sloppy

--- gist-project/ajaxAPI.js
+++ gist-project/ajaxAPI.js
@@ -24,20 +24,61 @@ function getLanguages(){
 
 // get all recent gists
 function getAllGists(numOfPages){
-    var pages = [];  // array to store search results
+    clearResults();
+    var pages = [];
+    var languages = getLanguages();
 
     for (var i=1; i <= numOfPages; i++)
     {
+	// This is the actual AJAX stuff here.  See docs at:
+	// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+
 	var ghr = null; // github response
 	ghr = new XMLHttpRequest();
-	ghr.open("GET", "https://api.github.com/gists/public?per_page=100&page=" + i, false);
-	ghr.send(null); // get back a string of json
-	var parsedResult = JSON.parse(ghr.responseText); // turn string into json object	    
-	pages.push(parsedResult); 
-    }
+	
+	ghr.open("GET", "https://api.github.com/gists/public?per_page=100&page=" + i, true);
 
-    return pages;
+	function parseGistsCallback(e){ 
+	    console.log("ghr:" + ghr.readyState);
+	    console.log("page length = " + pages.length);
+	    if (ghr.readyState == 4) {
+		console.log("got server response for page " + i);
+		var parsedResult = JSON.parse(ghr.responseText); 
+		pages.push(parsedResult); 
+		console.log("page length = " + pages.length);
+		if (pages.length >= 1){
+		    console.log("getting results");
+		    var results = parseGists(pages,languages,i);
+		    displayResults(results);
+		}
+		    
+		// return pages;
+	    }
+	}
+
+
+	ghr.send(null); // get back a string of json
+	ghr.onreadystatechange = parseGistsCallback; // note there is no () on the function
+
+    }
     // return parsedResult;
+
+
+    console.log("I'm done with gists");
+}
+
+function clearResults(){
+    var resultList = document.getElementById('searchResultsUL');
+    resultList.innerHTML="";  // clear the list if it's not already cleared
+}
+
+function displayResults(formattedResults){
+
+    var resultList = document.getElementById('searchResultsUL');
+
+    for (var r = 0; r < formattedResults.length; r++)
+	resultList.appendChild(formattedResults[r]); 
+
 }
 
 
@@ -47,20 +88,20 @@ function getAllGists(numOfPages){
    gistObjectArray is an array of objects, 
    allowedLanguages is an array of strings
 */
-function parseGists(gistObjectArray, allowedLanguages){  
+function parseGists(gistObjectArray, allowedLanguages, pageNumber){  
     results = []; 
     for (var j=0; j < gistObjectArray.length; j++){
 
 	// Make A Note of the Page We're On
 	var pageHeader = document.createElement('li');
-	pageHeader.innerHTML = "<b>page" + (j+1)  + "</b>";
+	pageHeader.innerHTML = "<b>page" + (pageNumber+1)  + "</b>";
 	results.push(pageHeader);
 
 	// loop over object and push results into array
 	for (var i in gistObjectArray[j]){  // gistObject is an object with an array of objects
      	    var r = document.createElement('li');
      	    r.style.clear = 'both';
-	    var nestedResult = gistObjectArray[j][i];
+	    var nestedResult = gistObjectArray[j][i]; console.log(nestedResult);
 	    var description = nestedResult['description']; // get description of current object
 	    var htmlURL = nestedResult['html_url'];  // get URL of current object
 	    var theseFiles = nestedResult['files'];  // object of files
@@ -87,25 +128,15 @@ function parseGists(gistObjectArray, allowedLanguages){
     return results; // an array of divs containing linked descriptions to gists filtered by allowed languages
 }
 
-function displayResults(formattedResults){
-
-    var resultList = document.getElementById('searchResultsUL');
-    resultList.innerHTML="";  // clear the list if it's not already cleared
-
-    for (var r = 0; r < formattedResults.length; r++)
-	resultList.appendChild(formattedResults[r]); 
-
-}
-
 
 function loadGists() {
-    var languages = getLanguages(); console.log(languages); // array of strings
-    var numOfPages = document.getElementById('numOfPagesSelection').value; console.log(numOfPages); // int
-    var gists = getAllGists(numOfPages);     console.log(gists); // array of objects
-    var results = parseGists(gists,languages);   console.log(results);  // array of html elements (li currently)
-    for (var r = 0; r < results.length; r++)
-	console.log(results[r]);
-    displayResults(results); // render array of html results to screen
+    // var languages = getLanguages(); console.log(languages); // array of strings
+    var numOfPages = document.getElementById('numOfPagesSelection').value; console.log("numOfPages="+numOfPages); // int
+    getAllGists(numOfPages); //    console.log(gists); // array of objects
+    //var results = parseGists(gists,languages);   console.log(results);  // array of html elements (li currently)
+    // for (var r = 0; r < results.length; r++)
+    // 	console.log(results[r]);
+    // displayResults(results); // render array of html results to screen
 };
 
 
