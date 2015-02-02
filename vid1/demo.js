b646a395ec2689796d4b45327ba66d50ce74aaea
greg <gregorio.jp.mitchell@gmail.com>
2015-02-01 22:13:44 -0700

    Deleted the aforementioned files/folders from git's tracking system.

--- vid1/demo.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    // note there is a typo in the video, document.getElementByID where D is capital results in an undefined type error
-    document.getElementById('output').innerHTML = 'test'; 
-};
-
-function saveDemoInput() {
-    console.log(document.getElementsByName('demo-input')[0].value);
-    };
